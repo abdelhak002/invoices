bd8fef8a258d9f94283a83587eaff160155025ed
Darbeida Abdelhak <darbeida.abdelhak@gmail.com>
2023-10-28 23:08:45 +0100

    feat: use `indexedDB` functions

--- src/TodoForm.tsx
+++ src/TodoForm.tsx
@@ -1,29 +1,38 @@
 import { Dispatch, SetStateAction, useState } from 'react'
 import { Todo } from './App'
+import { addData, openDatabase } from './services/idb'
 
 const TodoForm = ({ setTodoList }: { setTodoList: Dispatch<SetStateAction<Todo[]>> }) => {
   const [title, setTitle] = useState<string>('')
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  async function exampleAddDataUsage(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     if (!title) return
-    setTodoList((prevTodos) => [...prevTodos, { id: prevTodos.length + 1, title, completed: false }])
-    setTitle('')
+    try {
+      const db = await openDatabase()
+      const newTodo: Todo = { id: new Date().getTime(), title: title, completed: false }
+      const res = await addData<Todo>(db, 'todos', newTodo)
+      console.log('Data added successfully.')
+      console.log({ res })
+      setTodoList((prevTodos) => [...prevTodos, newTodo])
+    } catch (error) {
+      console.error('Error adding data:', error)
+    }
   }
 
   return (
-    <form onSubmit={onSubmit} className="w-full">
-      <div className="mb-6 flex w-full space-x-2">
+    <form onSubmit={exampleAddDataUsage} className="w-full">
+      <div className="mb-6 flex w-full space-x-2 px-2 md:px-0">
         <input
           type="text"
           id="large-input"
-          className="sm:text-md flex-auto rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-lg text-white placeholder-gray-400 outline-none focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+          className="sm:text-md flex-auto rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-lg text-white placeholder-gray-400 outline-none focus:border-[rgb(255,88,88)] focus:ring focus:ring-[rgb(255,88,88)] focus:ring-opacity-50"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
         />
         <button
           disabled={!title}
           type="submit"
-          className="w-full disabled:bg-opacity-60 disabled:text-opacity-60 disabled:cursor-not-allowed rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 bg-blue-600 enabled:hover:bg-blue-700 focus:ring-blue-800 sm:w-auto"
+          className="rounded-lg bg-[rgb(255,88,88)] px-3 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-red-800 enabled:hover:bg-red-500 disabled:cursor-not-allowed disabled:bg-opacity-60 disabled:text-opacity-60 sm:w-auto xl:px-5"
         >
           Submit
         </button>
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,5 +1,6 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect } from 'react'
 import { Todo } from './App'
+import { deleteItem, displayData, openDatabase, updateItem } from './services/idb'
 
 interface TodoListProps {
   todoList: Todo[]
@@ -7,24 +8,52 @@ interface TodoListProps {
 }
 
 const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
-  function handleToggleComplete(id: number) {
+  async function getAllTodos() {
+    const db = await openDatabase()
+    try {
+      const todos = await displayData<Todo>(db, 'todos')
+      console.log({ todos })
+      setTodoList(todos)
+    } catch (error) {
+      console.error('Error displaying data:', error)
+    }
+  }
+
+  useEffect(() => {
+    getAllTodos()
+  }, [])
+
+  async function handleToggleComplete(id: number, todo: Todo) {
+    const db = await openDatabase()
+    const updatedData = { ...todo, completed: !todo.completed }
+    const updatedTodos = await updateItem(db, 'todos', id, updatedData)
+    console.log({ updatedTodos, msg: 'updated' })
     setTodoList((prevTodos) => prevTodos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)))
   }
 
+  async function handleDeleteItem(id: number) {
+    const db = await openDatabase()
+    const deleteItemRes = await deleteItem(db, 'todos', id)
+    console.log({ deleteItemRes, msg: 'Item Deleted' })
+    getAllTodos()
+  }
+
   return (
-    <div className="w-full rounded-md bg-gray-700">
+    <div className="w-[97%] md:w-full rounded-md bg-gray-700">
       <ul className="w-full divide-y divide-gray-600">
-        {todoList.map((todo) => (
-          <li key={todo.id} className="flex items-center justify-between p-4 text-xl">
-            <span className="text-gray-100">{todo.title}</span>
-            <input
-              type="checkbox"
-              defaultChecked={todo.completed}
-              className="h-5 w-5 accent-indigo-500 "
-              onChange={() => handleToggleComplete(todo.id)}
-            />
-          </li>
-        ))}
+        {todoList.length === 0 && <li className="p-4 text-xl text-white">There are no Todos</li>}
+        {todoList.length > 0 &&
+          todoList.map((todo) => (
+            <li key={todo.id} className="relative flex items-center justify-between space-x-2 pr-4 text-xl">
+              <button onClick={() => handleDeleteItem(todo.id)} type="button" className="h-12 px-3 text-red-500 hover:bg-gray-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+              <span className={`${todo.completed && 'text-red-400 line-through'} flex-auto text-gray-100`}>{todo.title}</span>
+              <input type="checkbox" defaultChecked={todo.completed} className="h-5 w-5 accent-[rgb(255,88,88)] " onChange={() => handleToggleComplete(todo.id, todo)} />
+            </li>
+          ))}
       </ul>
     </div>
   )

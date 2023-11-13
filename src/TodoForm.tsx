966afe6bcb7ff8c51a917578bed3475aadc7ac04
Darbeida Abdelhak <darbeida.abdelhak@gmail.com>
2023-11-13 15:19:43 +0100

    style: increase the `z-index`, background colors

--- src/TodoForm.tsx
+++ src/TodoForm.tsx
@@ -20,19 +20,19 @@ const TodoForm = ({ setTodoList }: { setTodoList: Dispatch<SetStateAction<Todo[]
   }
 
   return (
-    <form onSubmit={exampleAddDataUsage} className="w-full">
+    <form onSubmit={exampleAddDataUsage} className="w-full z-10">
       <div className="mb-6 flex w-full space-x-2 px-2 md:px-0">
         <input
           type="text"
           id="large-input"
-          className="sm:text-md flex-auto rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-lg text-white placeholder-gray-400 outline-none focus:border-[rgb(255,88,88)] focus:ring focus:ring-[rgb(255,88,88)] focus:ring-opacity-50"
+          className="sm:text-md flex-auto rounded-lg border border-gray-600 bg-gray-800 p-2.5 text-lg text-white placeholder-gray-400 outline-none focus:border-[rgb(255,88,88)] focus:ring focus:ring-[rgb(255,88,88)] focus:ring-opacity-50"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
         />
         <button
           disabled={!title}
           type="submit"
-          className="rounded-lg bg-[rgb(255,88,88)] px-3 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-red-800 enabled:hover:bg-red-500 disabled:cursor-not-allowed disabled:bg-opacity-60 disabled:text-opacity-60 sm:w-auto xl:px-5"
+          className="rounded-lg bg-[rgb(255,88,88)] px-3 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-red-800 enabled:hover:bg-red-500 disabled:cursor-not-allowed disabled:bg-opacity-80 disabled:text-opacity-95 sm:w-auto xl:px-5 transition-all ease-in-out duration-200"
         >
           Submit
         </button>

6ebce704a5598c586d8d5061a0403b749c086fb2
Darbeida Abdelhak <darbeida.abdelhak@gmail.com>
2023-11-13 15:18:25 +0100

    feat: implement the stars to be shown in backgound
    - fix: create `todos objectStores` if it does not exist in first render

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction, useEffect } from 'react'
 import { Todo } from './App'
 import { deleteItem, displayData, openDatabase, updateItem } from './services/idb'
+import StarBackground from './StarBackground'
 
 interface TodoListProps {
   todoList: Todo[]
@@ -9,10 +10,9 @@ interface TodoListProps {
 
 const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
   async function getAllTodos() {
-    const db = await openDatabase()
+    const db = await openDatabase([{ name: 'todos', keyPath: 'id', indexes: [{ name: 'title', keyPath: 'title', options: { unique: false } }] }])
     try {
       const todos = await displayData<Todo>(db, 'todos')
-      console.log({ todos })
       setTodoList(todos)
     } catch (error) {
       console.error('Error displaying data:', error)
@@ -39,23 +39,28 @@ const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
   }
 
   return (
-    <div className="w-[97%] md:w-full rounded-md bg-gray-700">
-      <ul className="w-full divide-y divide-gray-600">
-        {todoList.length === 0 && <li className="p-4 text-xl text-white">There are no Todos</li>}
-        {todoList.length > 0 &&
-          todoList.map((todo) => (
-            <li key={todo.id} className="relative flex items-center justify-between space-x-2 pr-4 text-xl">
-              <button onClick={() => handleDeleteItem(todo.id)} type="button" className="h-12 px-3 text-red-500 hover:bg-gray-500">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-              <span className={`${todo.completed && 'text-red-400 line-through'} flex-auto text-gray-100`}>{todo.title}</span>
-              <input type="checkbox" defaultChecked={todo.completed} className="h-5 w-5 accent-[rgb(255,88,88)] " onChange={() => handleToggleComplete(todo.id, todo)} />
-            </li>
-          ))}
-      </ul>
-    </div>
+    <>
+      <StarBackground todoCount={todoList.length} />
+      <div className="z-10 w-[97%] rounded-md bg-gray-800 md:w-full">
+        <ul className="w-full divide-y divide-gray-700">
+          {todoList.length === 0 && <li className="p-4 text-center text-xl text-white">There are no Todos</li>}
+          {todoList.length > 0 &&
+            todoList
+              .sort((a, b) => b.id - a.id)
+              .map((todo) => (
+                <li key={todo.id} className="relative flex items-center justify-between space-x-2 pr-4 text-xl">
+                  <button onClick={() => handleDeleteItem(todo.id)} type="button" className="my-3 rounded-md p-0.5 text-red-500 hover:bg-gray-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                  <span className={`${todo.completed && 'text-red-400 line-through'} flex-auto text-gray-100`}>{todo.title}</span>
+                  <input type="checkbox" checked={todo.completed} className="h-5 w-5 accent-[rgb(255,88,88)] " onChange={() => handleToggleComplete(todo.id, todo)} />
+                </li>
+              ))}
+        </ul>
+      </div>
+    </>
   )
 }
 
